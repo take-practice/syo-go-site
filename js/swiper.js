bbc8100be45fd613e3295fbd42fa73d4e1362803
take-practice <78462474+take-practice@users.noreply.github.com>
2021-02-10 14:34:38 +0900

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,27 @@
  var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3.5,
+        slidesPerView: 4,
         spaceBetween: 56,
+        
         centeredSlides: true,
         initialSlide: 1,
         loop: true,
         autoplay: {
-            delay: 3000,
+            delay: 5000,
             stopOnLastSlide: false,
             disableOnInteraction: false,
             reverseDirection: false
         },
-        speed: 1000
+        speed: 1000,
+        breakpoints: {
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1023: {
+                slidesPerView: 3.5,
+                spaceBetween: 20,
+            }
+        }
 });
 
 AOS.init()
